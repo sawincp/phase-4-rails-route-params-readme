60393d294972b3d709e148436155750b3a444340
Corey Sawin <sawincp@gmail.com>
2023-07-21 15:10:17 -0400

    commit tree 692a40e9ce100bdc44e694625550bcd0d5f36083

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,13 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    #find a cheese using ID from URL
+    #send json reponse using that cheese obj
+
+    cheese = Cheese.find_by(id: params[:id])
+    render json: cheese
+
+  end
+
 end
